6577432d10cb28dee69d50d91dfe1130c7786f9e
khaleddiab98 <60040123+khaleddiab98@users.noreply.github.com>
2022-05-16 20:24:29 +0200

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
 eb setenv AWS_ACCESS_KEY_ID = $AWS_ACCESS_KEY_ID
-    && setenv AWS_SECRET_ACCESS_KEY = $AWS_SECRET_ACCESS_KEY
-        && setenv AWS_DEFAULT_REGION = $AWS_DEFAULT_REGION
+    && eb setenv AWS_SECRET_ACCESS_KEY = $AWS_SECRET_ACCESS_KEY
+        && eb setenv AWS_DEFAULT_REGION = $AWS_DEFAULT_REGION
             && eb init --region us-east-1 udagram-api
                 && eb use udagram-api-dev
                     && eb deploy udagram-api-dev
